942bbacc4ab767740b8820e46bc12077985366f9
em_wilson <emilyellenwilson@gmail.com>
2022-03-24 21:46:29 -0400

    Creating expand and collapse Q&A FAQ page

--- js/script.js
+++ js/script.js
@@ -11,3 +11,19 @@ function toggleMenu() {
 // Set Date in Footer
 const date = document.getElementById("date");
   date.innerHTML = new Date().getFullYear(); 
+
+
+//On FAQ Page - Display answer upon click
+
+//First, grab button elements
+const btns = document.querySelectorAll(".qa-button"); 
+// console.log(btns);
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function(e){
+    const toggleAnswer = e.currentTarget.parentElement.parentElement; 
+    console.log(toggleAnswer);
+    toggleAnswer.classList.toggle("show-answer"); 
+  })
+})
+
